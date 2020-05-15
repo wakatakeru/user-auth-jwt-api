974c2d31b63bf662460d288c57c5cde7c307d65d
wakatakeru <s19g460@stu.kagawa-u.ac.jp>
2020-05-16 00:19:08 +0900

    add sql handler functions

--- infrastructure/sql_handler.go
+++ infrastructure/sql_handler.go
@@ -6,10 +6,19 @@ import (
 	"os"
 
 	"github.com/go-sql-driver/mysql"
+	"github.com/wakatakeru/user-auth-jwt-api/interfaces/database"
 )
 
 type SqlHandler struct {
 	Conn *sql.DB
+}
+
+type SqlResult struct {
+	Result sql.Result
+}
+
+type SqlRow struct {
+	Rows *sql.Rows
 }
 
 func NewSqlHandler() *SqlHandler {
@@ -31,4 +40,44 @@ func NewSqlHandler() *SqlHandler {
 	sqlHandler := new(SqlHandler)
 	sqlHandler.Conn = conn
 	return sqlHandler
+}
+
+func (handler *SqlHandler) Execute(statement string, args ...interface{}) (database.SqlResult, error) {
+	sqlResult := SqlResult{}
+	result, err := handler.Conn.Query(statement, args...)
+	if err != nil {
+		return result, err
+	}
+	sqlResult.Result = result
+	return sqlResult, nil
+}
+
+func (handler *SqlHandler) Query(statement string, args ...interface{}) (database.SqlRow, error) {
+	rows, err := handler.Conn.Query(statement, args...)
+	if err != nil {
+		return new(SqlRow), err
+	}
+	row := new(SqlRow)
+	row.Rows = rows
+	return row, nil
+}
+
+func (r SqlResult) LastInsertedId() (int64, error) {
+	return r.Result.LastInsertId()
+}
+
+func (r SqlResult) RowsAffected() (int64, error) {
+	return r.Result.RowsAffected()
+}
+
+func (r SqlRow) Scan(dest ...interface{}) error {
+	return r.Rows.Scan(dest...)
+}
+
+func (r SqlRow) Next() bool {
+	return r.Rows.Next()
+}
+
+func (r SqlRow) Close() bool {
+	return r.Rows.Close()
 }
