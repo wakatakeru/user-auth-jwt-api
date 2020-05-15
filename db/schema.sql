1cdb075e7e057569962d10adb38fef6e4acd11f3
wakatakeru <s19g460@stu.kagawa-u.ac.jp>
2020-05-16 00:41:08 +0900

    fix database schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,7 @@
 -- Create Table
-CREATE DATABASE userauth.users (
+CREATE TABLE userauth.users (
   id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-  name VARCHAR(64) NOT NULL,
+  name VARCHAR(64) NOT NULL UNIQUE,
   display_name VARCHAR(64),
   email VARCHAR(128),
   password VARCHAR(512)
