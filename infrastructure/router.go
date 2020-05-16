b825167eaa9f7788766162071ae792b8bf75b522
wakatakeru <s19g460@stu.kagawa-u.ac.jp>
2020-05-17 02:30:57 +0900

    add users new endpoint to router

---
+++ infrastructure/router.go
@@ -0,0 +1,25 @@
+package infrastructure
+
+import (
+	"github.com/gin-contrib/cors"
+	gin "github.com/gin-gonic/gin"
+	
+	"github.com/wakatakeru/refmag-index-api/interfaces/controllers"
+)
+
+var Router *gin.Engine
+
+func init() {
+	router := gin.Default()
+
+	// Config for CORS (AllowOrigins for development environment)
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://localhost:3000"}
+	router.Use(cors.New(config))
+
+	userController := controllers.NewUserController(NewSqlHandler())
+
+	router.POST("/users", func(c *gin.Context) {userController.Create(c)})
+
+	Router = router
+}
